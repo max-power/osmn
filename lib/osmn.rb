04eb3e2b52c30085c663fd91f13619d4b1107f65
Kevin <kevin.melchert@gmail.com>
2017-11-12 10:09:59 +0100

    implement lookup

--- lib/osmn.rb
+++ lib/osmn.rb
@@ -4,6 +4,7 @@ require 'ostruct'
 require 'osmn/request'
 require 'osmn/search'
 require 'osmn/reverse'
+require 'osmn/lookup'
 require 'osmn/version'
 
 module OSMN
@@ -15,5 +16,9 @@ module OSMN
 
   def reverse_geocode(lat, lon, **params)
     Reverse.new(params.merge(lat: lat, lon: lon)).call
+  end
+  
+  def lookup(*osm_ids, **params)
+    Lookup.new(params.merge(osm_ids: osm_ids.join(','))).call
   end
 end
---
+++ lib/osmn/lookup.rb
@@ -0,0 +1,9 @@
+module OSMN
+  class Lookup < Search
+    private
+
+    def path
+      '/lookup'
+    end
+  end
+end
